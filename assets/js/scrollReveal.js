aeb99193f4aeb7827db0edd8fe2c667eef6a5cef
Fabiano <7fabiano.silva@gmail.com>
2022-07-05 15:14:10 -0300

    Implementação da lib ScrollReveal.js

---
+++ assets/js/scrollReveal.js
@@ -0,0 +1,14 @@
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.scroll-effect-1s',{
+    duration: 1000,
+});
+
+sr.reveal('.scroll-effect-3s',{
+    duration: 3000,
+});
+
+sr.reveal('.scroll-effect-4s',{
+    duration: 4000,
+});
